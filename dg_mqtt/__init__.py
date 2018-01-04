eff3a8ca2e94f24986fd7f5cff67ef5287d210cc
K4r1 <karl@morteo.it>
2018-01-04 19:03:18 +0100

    Remove dg_mqtt as it was moved to src/dg_mqtt

--- dg_mqtt/__init__.py
+++
@@ -1,7 +0,0 @@
-from dg_mqtt.Gateway import Gateway
-from dg_mqtt.Device import Device
-from dg_mqtt.SwitchDevice import SwitchDevice
-from dg_mqtt.TriggerDevice import TriggerDevice
-from dg_mqtt.TransmitterDevice import TransmitterDevice
-from dg_mqtt.DHTDevice import DHTDevice
-from dg_mqtt.MoistureSensorDevice import MoistureSensorDevice
