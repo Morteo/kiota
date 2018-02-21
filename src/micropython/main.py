eecd49b34692aee114b269001d3426ad04e48c7d
K4r1 <karl@morteo.it>
2018-02-21 19:02:23 +0100

    Memory optimization

--- src/micropython/main.py
+++ src/micropython/main.py
@@ -1,40 +1,37 @@
-import json
-from dg_mqtt.Gateway import Gateway
-import dg_mqtt.Util as Util
+from kiota.Gateway import Gateway
+#import kiota.Util as Util
 
 def connectWifi(SSID, password):
+  
     import network
-
+    
     sta_if = network.WLAN(network.STA_IF)
     ap_if = network.WLAN(network.AP_IF)
     if ap_if.active():
         ap_if.active(False)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print("connecting to network...")
         sta_if.active(True)
         sta_if.connect(SSID, password)
         while not sta_if.isconnected():
             pass
-    print('Network configuration:', sta_if.ifconfig())
-
+    print("Network configuration: {}".format(sta_if.ifconfig()))
 
 try:
-  config = Util.loadConfig('config/config.json')
-  secret = Util.loadConfig('config/__secrets__.json')
-  config = Util.mergeConfig(config,secret)
-  #  print("WiFi config: {}".format(json.dumps(config)))
-  connectWifi(config['WiFi']['SSID'], config['WiFi']['password'])
-
-  Gateway().start()
+  from kiota.Configurator import Configurator
+  config = Configurator.load()
+#  print(config)
+  connectWifi(config["wifi"]["SSID"], config["wifi"]["password"])
+  Gateway(config).start()
+  
 except Exception as e:
   import sys
   sys.print_exception(e)
 
-# Gateway received a ../Gateway/exit message from MQTT server or encountered an unanticipated excpetion
+# Gateway received a ../exit message from MQTT server or encountered an unanticipated excpetion
 
-import webrepl
+#import webrepl
 # first time use of webREPL set it up 
 #import webrepl_setup
 #webrepl.start()
 #webrepl._webrepl.password("<password>")
-
